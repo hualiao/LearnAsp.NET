fb34fb42350e9ee384d0763bc3a3ace71bead5eb
liaohua <liaohua1990@hotmail.com>
2015-06-03 00:41:14 +0800

    Add CodeTimer

---
+++ CSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using CSharp.CodeTimer;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CodeTimer.CodeTimer.Time("Thread Sleep", 1, () => { Thread.Sleep(3000); });
+            CodeTimer.CodeTimer.Time("Empty Method", 10000000, () => { });
+
+            Console.ReadKey();
+        }
+    }
+}
