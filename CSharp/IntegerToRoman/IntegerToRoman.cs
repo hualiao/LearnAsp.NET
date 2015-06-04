8ca8b1660e7ec161a1cbce179e34463656d4fdcf
liaohua <liaohua1990@hotmail.com>
2015-06-05 00:50:05 +0800

    Add Integer To Roman

---
+++ CSharp/IntegerToRoman/IntegerToRoman.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp.IntegerToRoman
+{
+    public class RomanNumerals
+    {
+        private class Denomination
+        {
+            public int Value { get; set; }
+            public string Symbol { get; set; }
+        }
+
+        private static readonly Denomination[] table = 
+        {
+            new Denomination { Value = 1000, Symbol = "M" },
+            new Denomination { Value = 900, Symbol = "CM" },
+            new Denomination { Value = 500, Symbol = "D" },
+            new Denomination { Value = 400, Symbol = "CD" },
+            new Denomination { Value = 100, Symbol = "C" },
+            new Denomination { Value = 90, Symbol = "XC" },
+            new Denomination { Value = 50, Symbol = "L" },
+            new Denomination { Value = 40, Symbol = "XL" },
+            new Denomination { Value = 10, Symbol = "X" },
+            new Denomination { Value = 9, Symbol = "IX" },
+            new Denomination { Value = 5, Symbol = "V" },
+            new Denomination { Value = 4, Symbol = "IV" },
+            new Denomination { Value = 1, Symbol = "I" }
+        };
+
+        public static string Translate(int number)
+        {
+            var result = new StringBuilder();
+
+            int remainder = number;
+
+            //For single-dimensional arrays travel starting with index 0 and ending with index Length - 1
+            foreach (var current in table)
+            {
+                while (remainder >= current.Value)
+                {
+                    result.Append(current.Symbol);
+                    remainder -= current.Value;
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+
+}
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading;
 
 using CSharp.CodeTimer;
+using CSharp.IntegerToRoman;
 
 namespace CSharp
 {
@@ -14,6 +15,13 @@ namespace CSharp
         {
             CodeTimer.CodeTimer.Time("Thread Sleep", 1, () => { Thread.Sleep(3000); });
             CodeTimer.CodeTimer.Time("Empty Method", 10000000, () => { });
+
+            //check result here http://romannumerals.babuo.com/roman-numerals-1-5000
+            Console.WriteLine(RomanNumerals.Translate(2015));
+            Console.WriteLine(RomanNumerals.Translate(2048));
+            Console.WriteLine(RomanNumerals.Translate(2049));
+            Console.WriteLine(RomanNumerals.Translate(2050));
+            Console.WriteLine(RomanNumerals.Translate(2051));
 
             Console.ReadKey();
         }
