aed8d4684c55edfb0f76a2c609e5ce1c819302dc
liaohua <liaohua1990@hotmail.com>
2015-07-14 23:56:21 +0800

    Add more majority element implement

--- CSharp/Utility/MajorityElement.cs
+++ CSharp/Utility/MajorityElement.cs
@@ -11,6 +11,8 @@ namespace CSharp.Utility
     /// </summary>
     public class MajorityElement
     {
+        static Random _rnd = new Random();
+
         public static Tuple<bool, int, int> HasMajorityElement(int[] array)
         {
             return array
@@ -58,6 +60,134 @@ namespace CSharp.Utility
             .FirstOrDefault(g => seqLength / g.freq < 2);
 
             return m != null ? m.num : -1;
+        }
+
+        static void RandomizeArray(int[] a)
+        {
+
+            for (int i = a.Length - 1; i > 0; i--)
+            {
+                int pos = _rnd.Next(i + 1);
+                int x = a[pos];
+                a[pos] = a[i];
+                a[i] = x;
+            }
+
+        }
+
+        static int[] GenerateArray(int n)
+        {
+
+            int value = 1;
+            int pos = 0;
+            int[] a = new int[n];
+
+            while (pos < n)
+            {
+                int count = _rnd.Next(n - pos) + 1;
+                while (count-- > 0)
+                    a[pos++] = value;
+                value++;
+            }
+
+            RandomizeArray(a);
+
+            return a;
+
+        }
+        static void PrintArray(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+                Console.Write("{0,3}", a[i]);
+            Console.WriteLine();
+        }
+
+        static int GetCountForValue(int[] a, int x)
+        {
+
+            int count = 0;
+
+            for (int i = 0; i < a.Length; i++)
+                if (a[i] == x)
+                    count++;
+                else
+                    count--;
+
+            return count;
+
+        }
+
+
+        static int FindMajorityElement(int[] a)
+        {
+
+            int count = 1;
+            int candidate = a[0];
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] == candidate)
+                {
+                    count++;
+                }
+                else if (count == 0)
+                {
+                    candidate = a[i];
+                    count = 1;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+
+            if (count > 0)
+                count = GetCountForValue(a, candidate);
+
+            if (count > 0)
+                return candidate;
+
+            return -1;
+
+        }
+
+        /// <summary>
+        /// Ref: http://www.codinghelmet.com/?path=exercises/majority-element
+        /// </summary>
+        /// <param name="args"></param>
+        public static void MajorityByOnO1(string[] args)
+        {
+
+            while (true)
+            {
+
+                Console.Write("n=");
+                int n = int.Parse(Console.ReadLine());
+
+                if (n <= 0)
+                    break;
+
+                int[] a = GenerateArray(n);
+
+                PrintArray(a);
+
+                int majority = FindMajorityElement(a);
+
+                if (majority < 0)
+                    Console.WriteLine("No majority element.");
+                else
+                {
+                    int count = 0;
+                    for (int i = 0; i < n; i++)
+                        if (a[i] == majority)
+                            count++;
+                    Console.WriteLine("Majority number is {0} (occurring {1} times).", majority, count);
+                }
+            }
+
+            Console.Write("Press ENTER to continue... ");
+            Console.ReadLine();
+
         }
     }
 }
