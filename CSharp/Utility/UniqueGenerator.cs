14e3dda6d6caf8ba1fb5b121221c98a8e4fc642c
liaohua <liaohua1990@hotmail.com>
2015-08-04 00:27:01 +0800

    Unique String Generator

---
+++ CSharp/Utility/UniqueGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp.Utility
+{
+    /// <summary>
+    /// Ref: http://madskristensen.net/post/generate-unique-strings-and-numbers-in-c
+    /// </summary>
+    public class UniqueGenerator
+    {
+        /// <summary>
+        /// Ref: http://stackoverflow.com/questions/7513391/newguid-vs-system-guid-newguid-tostringd
+        /// </summary>
+        /// <returns></returns>
+        public static string GUID()
+        {
+            Guid guid = Guid.NewGuid();
+            // "N" - xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx (32 digits)
+            // "D" - xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx (32 digits separated by hyphens)
+            // "B" - {xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx} (same as "D" with addition of braces)
+            // "P" - (xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx) (same as "D" with addition of parentheses)
+            // "X" - {0x00000000,0x0000,0x0000,{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00}}
+            return guid.ToString();
+            //return guid.ToString("N");
+            //return guid.ToString("D");
+        }
+
+        private string GenerateId()
+        {
+            long i = 1;
+            foreach (byte b in Guid.NewGuid().ToByteArray())
+            {
+                i *= ((int)b + 1); // plus 1 remove 0 value
+            }
+            return string.Format("{0:x}", i - DateTime.Now.Ticks); // Ticks return long, 1 Ticks = 100 nanosecond 
+        }
+
+        private long GenerateId()
+        {
+            byte[] buffer = Guid.NewGuid().ToByteArray();
+            return BitConverter.ToInt64(buffer, 0);
+        }
+    }
+}
