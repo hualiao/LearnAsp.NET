f2084b75b11358c60ff053c9778a6004737e4bf8
liaohua <liaohua1990@hotmail.com>
2015-08-31 00:26:20 +0800

    List All Anagrams SqlCommand demo

---
+++ CSharp/Algorithm/LiatAllAnagrams.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace CSharp.Algorithm
+{
+    // Ref: http://geekswithblogs.net/BlackRabbitCoder/archive/2015/08/04/solution-to-little-puzzlersndashldquolist-all-anagrams-in-a-wordrdquo.aspx
+
+    public class WordFileDao : IWordDao
+    {
+        public string FileName { get; private set; }
+
+        public WordFileDao(string fileName)
+        {
+            FileName = fileName;
+        }
+
+        public IEnumerable<string> RetrieveWords()
+        {
+            // you can start enumerating the collection stirngs
+            // make sure to remove any accidental space and normalize to lower case
+            return File.ReadLines(FileName).Select(l => l.Trim().ToLower());
+        }
+    }
+
+    public class AnagramFinder
+    {
+        private readonly ILookup<string, string> _anagramLookup;
+
+        public AnagramFinder(IWordDao dao)
+        {
+            // construct the lookup at initialization using the
+            // dictionary words with the sorted letters as the key
+            _anagramLookup = dao.RetrieveWords().ToLookup(
+                k => string.Concat(k.OrderBy(c => c)));
+        }
+
+        public IList<string> FindAnagrams(string word)
+        {
+            // at lookup time, sort the input word as key,
+            // and return all words (minus the input word) in the sequence
+            string input = word.ToLower();
+            string key = string.Concat(input.OrderBy(c => c));
+
+            return _anagramLookup[key].Where(w => w != input).ToList();
+        }
+    }
+
+    interface IWordDao 
+    {
+        IEnumerable<string> RetrieveWords();
+    }
+}
---
+++ CSharp/SQLHelper/SqlCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CSharp.SQLHelper
+{
+    class SqlCommand
+    {
+        private static string _connection;
+
+        public static void BatchInsert()
+        {
+            // how to get App Connecting string 
+            // Ref:http://www.codeproject.com/Tips/416198/How-to-get-Connection-String-from-App-Config-in-Cs
+            _connection = System.Configuration.ConfigurationManager.ConnectionStrings["AliCloud"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(_connection))
+            {
+                SqlCommand command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandText = "insert into Person(Name,Gender,Address) values(@Name1,@Gender1,@Address1); insert into Person(Name,Gender,Address) values(@Name1,@Gender1,@Address2);";
+                SqlParameter[] parameters = { 
+                                                new SqlParameter("@Name1","liao"),
+                                                new SqlParameter("@Gender1",1),
+                                                new SqlParameter("@Address1","厦门"),
+                                                new SqlParameter("@Address2","福建")
+                                            };
+                command.Parameters.AddRange(parameters);
+                connection.Open();
+                int result = command.ExecuteNonQuery();
+            }
+        }
+    }
+}
