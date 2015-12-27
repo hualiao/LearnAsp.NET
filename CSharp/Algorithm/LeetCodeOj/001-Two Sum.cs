a07a9d0857f6168709976d2460a64e7f38e87a92
liaohua <liaohua1990@hotmail.com>
2015-12-28 00:36:02 +0800

    Two Sum

---
+++ CSharp/Algorithm/LeetCodeOj/001-Two Sum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp.Algorithm.LeetCodeOj
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Hashtable ht = new Hashtable();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int x = nums[i];
+                if (ht.ContainsKey(target - x))
+                {
+                    int[] rs = { (int)ht[target - x] + 1, i + 1 };
+                }
+                else
+                {
+                    ht[x] = i;
+                }
+            }
+            return null;
+        }
+
+        public int[] TwoSum1(int[] nums, int target)
+        {
+            Dictionary<int, int> dt = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dt.ContainsKey(target - nums[i]))
+                    return new int[] { dt[target - nums[i]] + 1, i + 1 };
+                else
+                {
+                    if (!dt.ContainsKey(target - nums[i]))
+                        dt.Add(nums[i], i);
+                }
+            }
+            return new int[2];
+        }
+    }
+}
